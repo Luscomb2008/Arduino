3f69a6f57165f22a1e20b8d63a8df8ff6d650399
Luscomb <02lucas.luscomb@gulllakecs.org>
2025-04-23 14:05:25 -0400

    fading sequentially with multiple LED's

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -1,40 +1,34 @@
-/*
-  Fade
+int blueled = 9;
+int greenled = 10;
+int clearled = 3;
+int redled = 5;
+int brightness = 0;
+int fadeAmount = 5;
 
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Fade/
-*/
-
-int led = 9;         // the PWM pin the LED is attached to
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
-
-// the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(blueled, OUTPUT);
+  pinMode(greenled, OUTPUT);
+  pinMode(clearled, OUTPUT);
+  pinMode(redled, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  fadeLED(blueled);
+  fadeLED(greenled);
+  fadeLED(clearled);
+  fadeLED(redled);
+}
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+void fadeLED(int ledPin) {
+  // Fade in
+  for (brightness = 0; brightness <= 255; brightness += fadeAmount) {
+    analogWrite(ledPin, brightness);
+    delay(30);
   }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+
+  // Fade out
+  for (brightness = 255; brightness >= 0; brightness -= fadeAmount) {
+    analogWrite(ledPin, brightness);
+    delay(30);
+  }
 }
