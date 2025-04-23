0953660ed8c84ff8f838f4ef285d5a46dde42e49
Luscomb <02lucas.luscomb@gulllakecs.org>
2025-04-23 13:27:35 -0400

    4 LED's blinking faster

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -16,25 +16,25 @@ void setup() {
 void loop() {
   // Blue LED on and off
   digitalWrite(blueLED, HIGH);  // turn the blue LED on
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   digitalWrite(blueLED, LOW);   // turn the blue LED off
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   
   // Red LED on and off
   digitalWrite(redLED, HIGH);   // turn the red LED on
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   digitalWrite(redLED, LOW);    // turn the red LED off
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   
   // Green LED on and off
   digitalWrite(greenLED, HIGH); // turn the green LED on
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   digitalWrite(greenLED, LOW);  // turn the green LED off
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
 
   // Clear LED on and off
   digitalWrite(clearLED, HIGH); // turn the clear LED on
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
   digitalWrite(clearLED, LOW);  // turn the clear LED off
-  delay(500);                    // wait for 0.5 seconds
+  delay(100);                    // wait for 0.5 seconds
 }
